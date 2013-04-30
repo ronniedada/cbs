97619e85c103ea4815d24ef326cbe45456480eed
ronniedada <ronniedada@gmail.com>
2013-04-29 17:09:53 -0700

    added os distribution by cluster chart

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,7 +4,8 @@ var CONFIG = {
 		'os_type': { 'name': 'OS Distribution', 'src': '/api/bar/ram/os_ram_sizes/?y_index=0', 'type': 'bar', 'args': {}},
 		'ram_sizes': { 'name': 'RAM Size', 'src': '/api/bar/ram/os_ram_sizes/?y_index=1', 'type': 'bar', 'args': {}},
 		'contact_by_date': { 'name': 'Contact By Date', 'src': '/api/line/overtime/contact_by_date/?y_index=0', 'type': 'line', 'args': {'xparse': 'time'}},
-		'browser_by_cluster': {'name': "Browser By Cluster", 'src': '/api/stackedbar/browser/nodes_counts/?group_level=2&y_index=0&x_index=1&range_index=0&range=8', 'type': 'stackedbar', 'args': {}}
+		'browser_by_cluster': {'name': "Browser By Cluster", 'src': '/api/stackedbar/browser/nodes_counts/?group_level=2&y_index=0&x_index=1&range_index=0&range=8', 'type': 'stackedbar', 'args': {}},
+		'os_by_cluster': {'name': "OS By Cluster", 'src': '/api/stackedbar/os/os_nodes/?group_level=2&y_index=1&x_index=0&range_index=1&range=8', 'type': 'stackedbar', 'args': {}}
 };
 
 function ChartCtrl($scope, $routeParams, $http) {
