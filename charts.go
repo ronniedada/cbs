52ad2722d57488cbb1579447f811e07e35e034f6
ronniedada <ronniedada@gmail.com>
2013-05-16 14:49:29 -0700

    used reducers for contact charts
    - obvious performance gain observed here

--- charts.go
+++ charts.go
@@ -35,50 +35,36 @@ func serveBarChart(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	histo := vr.histo()
+	calcHisto, err := strconv.ParseBool(r.FormValue("calc_histo"))
+
+	if err != nil {
+		calcHisto = false
+	}
+
+	var histo map[interface{}]int
+	if calcHisto {
+		yIndex := r.FormValue("y_index")
+		i, err = strconv.Atoi(yIndex)
+		if err != nil {
+			showError(w, r, "Unable to generate chart : missing yIndex", 404)
+			return
+		}
+		histo = vr.calcHistos()[i]
+	} else {
+		histo = vr.extractHisto()
+	}
+
 	if len(histo) == 0 {
 		showError(w, r, "oops, empty results", 404)
 		return
 	}
-	axes := []string{"x", "y"}
 
-	yIndex := r.FormValue("y_index")
-	i, _ = strconv.Atoi(yIndex)
-	vr.bar(histo[i], axes, w)
+	axes := []string{"x", "y"}
+	vr.bar(histo, axes, w)
 }
 
 func serveLineChart(w http.ResponseWriter, r *http.Request) {
-	ddoc, view := mux.Vars(r)["ddoc"], mux.Vars(r)["view"]
-
-	var args map[string]interface{}
-
-	groupLevel := r.FormValue("group_level")
-	i, err := strconv.Atoi(groupLevel)
-
-	if err != nil {
-		args = map[string]interface{}{"stale": "update_after"}
-	} else {
-		args = map[string]interface{}{"stale": "update_after",
-			"group_level": i}
-	}
-
-	vr, err := fetchView(ddoc, view, args)
-
-	if err != nil {
-		showError(w, r, err.Error(), 404)
-		return
-	}
-
-	histo := vr.histo()
-	if len(histo) == 0 {
-		showError(w, r, "oops, empty results", 404)
-		return
-	}
-	axes := []string{"x", "y"}
-
-	yIndex := r.FormValue("y_index")
-	i, _ = strconv.Atoi(yIndex)
-	vr.line(histo[i], axes, w)
+	serveBarChart(w, r)
 }
 
 func serveStackedBarChart(w http.ResponseWriter, r *http.Request) {
