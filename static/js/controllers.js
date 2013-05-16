91f360faca872b41095c63f34478f2a871d2e4e3
ronniedada <ronniedada@gmail.com>
2013-05-16 14:47:49 -0700

    spacing and todo

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,13 +1,14 @@
 'use strict';
 
+// TODO: move to a saparate file
 var CONFIG = {
 		'os_type': { 'name': 'OS Distribution', 'src': '/api/bar/ram/os_ram_sizes/?y_index=0',
 					 'type': 'bar', 'args': {}},
 		'ram_sizes': { 'name': 'RAM Size', 'src': '/api/bar/ram/os_ram_sizes/?y_index=1',
 			           'type': 'bar', 'args': {}},
-		'contact_by_date': { 'name': 'Contact By Date', 'src': '/api/line/overtime/contact_by_date/?y_index=0',
+		'contact_by_date': { 'name': 'Contact By Date', 'src': '/api/line/dev_overtime/contact_by_date/?group_level=1',
 							 'type': 'line', 'args': {'xparse': 'time'}},
-		'contact_by_day': {'name': "Contact By Day of Week", 'src': '/api/bar/overtime/contact_by_day_hour_size/?y_index=0',
+		'contact_by_day': {'name': "Contact By Day of Week", 'src': '/api/bar/dev_overtime/contact_by_day_hour_size/?group_level=1',
 							'type': 'bar', 'args': {'xparse': 'day'}},
 		'browser_by_cluster': {'name': "Browser By Cluster", 'src': '/api/stackedbar/browser/nodes_counts/?group_level=2&y_index=0&x_index=1&range_index=0&range=8',
 							   'type': 'stackedbar', 'args': {}},
--- static/js/directives.js
+++ static/js/directives.js
@@ -2,14 +2,14 @@
 
 var createSVG, updateChart, bar, line;
 
-var weekday=new Array(7);
-weekday[0]="Sunday";
-weekday[1]="Monday";
-weekday[2]="Tuesday";
-weekday[3]="Wednesday";
-weekday[4]="Thursday";
-weekday[5]="Friday";
-weekday[6]="Saturday";
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
 angular.module('cbs.directives', [])
   .directive('chart', [
